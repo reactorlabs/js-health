2e6fa645229bc58b099dad75e939b3a2201a31b8
P00kiE <celeste.hollenbeck@gmail.com>
2017-09-08 17:59:33 -0400

    todo in comment

--- stuff/type_sorter.js
+++ stuff/type_sorter.js
@@ -10,16 +10,21 @@ module.exports = {
 		let path = process.argv[3];
 		let num = Number.parseInt(process.argv[4]);
 		let projects = utils.listProjects(path, num);
+		var all_stats = [];
+		var combined_stats;
+		// console.log(projects);
 		for (let p of projects) {
+			// console.log(p);
 			utils.analyzeProjectTools(p);
 			utils.analyzeProjectDependencies(p);
 			utils.addMetaData(p);
 			labelProject(p);
+			all_stats.push([p.url, proj_stats(p)]);
 		};
+		console.log(all_stats);
+		combined_stats = combine_stats(all_stats);
 		getResults(projects);
-		var package_stats = get_stats(path);
-		console.log(package_stats);
-	}
+		}
 }
 
 const labels = {
@@ -146,6 +151,19 @@ const labels = {
 		"yeoman-generator"]		
 };
 
+function combine_stats(tups) {
+	var total_stats = {};
+	// console.log("combining");
+	for (let t of tups) {
+		if (Object.keys(t[1]).length !== 0) {
+			console.log(t);
+		}
+	}
+	/**
+	 * resume here. Combine all of the projects' require statements into one dictionary & then do the sorting.
+	 * Keep a record of which projects use which requires, too, to correlate them w/ labels.
+	 */
+};
 
 function labelProject(p) {
 	p.labels = {};
@@ -198,24 +216,38 @@ function getResults(ps) {
 };
 
 
-function get_stats(path) {
-	var js_files = get_js_files(path); 
+function proj_stats(proj) {
+	var js_files = get_js_files(proj.path, proj.path);
 	var require_stats = tally_require_stmts(js_files);
+	//return sort_tally(require_stats);
 	return require_stats;
+}
+
+function sort_tally(dict) {
+	var arr = [];
+	for (var key in dict) {
+		arr.push([dict[key], key]);
+	}
+	arr.sort(function(a, b) {return b[0] - a[0]});
+	return arr;
 }
 
 function tally_require_stmts(js_files) {
 	var pkgs = {};
-	for (let f of js_files) {
+	for (let tup of js_files) {
+		var f = tup[0];
+		var pkg = tup[1];
 		var file_text = fs.readFileSync(f, "utf8");
 		var patt = /^require\(['"]([\w-]+)['"]\)/gm;
 		var path;
 		while ((path = patt.exec(file_text)) !== null) {
-			if (pkgs[path[1]] !== undefined) {
-				pkgs[path[1]] = pkgs[path[1]] + 1;
+			var pname = path[1];
+			if (pkgs[pname] !== undefined) {
+				pkgs[pname][0] = pkgs[pname][0] + 1;
+				pkgs[pname][1].push(pkg);
 			}
 			else {
-				pkgs[path[1]] = 1;
+				pkgs[pname] = [1, [pkg]];			
 			}
 		}
 	}
@@ -223,7 +255,7 @@ function tally_require_stmts(js_files) {
 	return pkgs;
 };
 
-function get_js_files(dir) {
+function get_js_files(dir, proj) {
 	var lastchar = dir.substring(dir.length - 1, dir.length);
 	if (lastchar === "/") {
 		dir = dir.substring(0, dir.length - 1);
@@ -233,12 +265,12 @@ function get_js_files(dir) {
 	for (let f of files_in_path) {
 		var _f = dir.concat("/").concat(f);
 		if (fs.lstatSync(_f).isDirectory()) {
-			js_files.push.apply(js_files, get_js_files(_f));
+			js_files.push.apply(js_files, get_js_files(_f, proj));
 		}
 		else if (fs.lstatSync(_f).isFile()) {
 			var ext = _f.substring(_f.length - 3, _f.length);
 			if (ext === ".js") {
-				js_files.push(_f);
+				js_files.push([_f, proj]);
 			}
 		}
 		else {

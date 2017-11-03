196264c843c8ce06149f3e88bf6435cb5ca9e353
Peta <peta.maj82@gmail.com>
2017-11-03 19:00:43 +0100

    Writes stuff to disk, more robust.

--- stuff/downloader_api.js
+++ stuff/downloader_api.js
@@ -1,5 +1,21 @@
 const async = require("async");
 const request = require("request")
+const fs = require("fs");
+
+// helpers which should eventually go to utils
+
+/** Creates a directory asynchronously. 
+ */
+function mkdir(where, name, callback) {
+    fs.mkdir(where + "/" + name, (err) => {
+        if (err != "EEXIST")
+            callback(err);
+        else 
+            callback(null);
+    });
+}
+
+
 
 module.exports = {
 
@@ -8,6 +24,9 @@ module.exports = {
     },
 
     download : function(apiTokens) {
+        projectOutputDir = "c:/delete/jsdownload/projects";
+        snapshotOutputDir = "c:/delete/jsdownload/files";
+
         console.time("all");
         apiTokens_ = apiTokens;
         console.log("Initialized with " + apiTokens_.length + " Github API tokens...");
@@ -22,6 +41,10 @@ module.exports = {
             console.log("KTHXBYE");
             process.exit();
         }
+
+        setInterval(() => {
+            console.log("Q: " + Q.running() + "/" + Q.length() + " - files " + stats_files + ", snapshots: " + stats_snapshots);
+        }, 1000)
 /*
 
         // load the projects now
@@ -38,15 +61,18 @@ module.exports = {
     }
 };
 
+let stats_projects = 0;
+let stats_files = 0;
+let stats_snapshots = 0;
+
+let projectOutputDir = null;
+let snapshotOutputDir = null;
+
 let Q = null; 
-
-var nextProjectId_ = 0;
-
 
 /** Trampoline that performs the appropriate task from the main queue.
  */
 function Task(task, callback) {
-    console.log("Executing task " + task.kind);
     switch (task.kind) {
         case "project":
             return TaskProject(task, callback);
@@ -79,6 +105,43 @@ function IsValidFilename(filename) {
     return false;
 }
 
+function InitializeProjectPath(project, callback) {
+    // if the project path exists, then we do not need to do anything, call the callback immediately
+    if (project.path) {
+        callback(null);
+    } else {
+        // otherwise we first make sure we have the directories, starting with the subdir
+        let subdir = project.info.id % 2000;
+        mkdir(projectOutputDir, subdir, (err) => {
+            // TODO check error
+            // then followed by the actual project directory
+            mkdir(projectOutputDir + "/" + subdir, project.info.id, (err) => {
+                // TODO check error
+                // set the project path, and call itself again, this time actually storing the file
+                project.path = projectOutputDir + "/" + subdir + "/" + project.info.id;
+                callback(err);
+            });
+        });
+    }
+}
+
+function SaveProjectInfo(project, callback) {
+    fs.writeFile(project.path + "/project.json", JSON.stringify(project.info), (err) => {
+        callback(err);
+    });
+}
+
+function SaveCommit(project, commit, callback) {
+    // first make sure the path for the commit exists
+    let subdir = commit.hash.substr(0, 2);
+    mkdir(project.path, subdir, (err) => {
+        // TODO check error
+        fs.writeFile(project.path + "/" + subdir + "/" + commit.hash + ".json", JSON.stringify(commit), (err) => {
+            callback(err);
+        });
+    });
+}
+
 /**  */
 function TaskProject(task, callback) {
     // create the project
@@ -89,11 +152,13 @@ function TaskProject(task, callback) {
     };
     if (task.id !== undefined) {
         // TODO load the project information from disk and specify task's URL 
+    } else {
+        // make sure that 
     }
     // now get the metadata for the project 
-    project.url = "http://api.github.com/repos/" + task.url
+    project.url = "http://api.github.com/repos/" + task.url;
     APIRequest(project.url,
-        (response, result) => {
+        (error, response, result) => {
             let i = project.info
             // fill in the task project
             i.id = result.id;
@@ -117,23 +182,26 @@ function TaskProject(task, callback) {
             // if the project is fork and we have parent, store its id
             if (result.parent !== undefined)
                 i.parent = result.parent.id;
-            // now search the branch
-            Q.unshift({
-                kind : "branch",
-                branch : i.default_branch,
-                project : project
-            })
-            // TODO save the project info here
-            callback();
-        },
-        Error(callback)
+            // now we must make sure that the project path exists before we can start processing the branches
+            InitializeProjectPath(project, (err) => {
+                // TODO make sure there is no error
+                // mark the default branch for analysis
+                Q.unshift({
+                    kind : "branch",
+                    branch : i.default_branch,
+                    project : project
+                });
+                // save the project info, which also executes our callback
+                SaveProjectInfo(project, callback);
+            });
+        }
     );
 }
 
 function TaskBranch(task, callback) {
     let project = task.project
     APIRequest(project.url + "/branches/" + task.branch,
-        (response, result) => {
+        (error, response, result) => {
             // TODO what if the branch already exists?? 
             let branch = {
                 name : result.name,
@@ -147,8 +215,7 @@ function TaskBranch(task, callback) {
             })
             // output the branch info
             callback();
-        },
-        Error(callback)
+        }
     );
 }
 
@@ -167,7 +234,7 @@ function TaskCommit(task, callback) {
     // get information about the commit
 
     APIRequest(project.url + "/commits/" + commit.hash, 
-        (response, result) => {
+        (error, response, result) => {
             commit.date = result.commit.author.date;
             commit.message = result.commit.message;
             commit.comment_count = result.commit.comment_count;
@@ -194,13 +261,24 @@ function TaskCommit(task, callback) {
                 // only deal with files we are interested in
                 if (! IsValidFilename(f.filename))
                     continue;
+                // change in file permissions, not interested for us
+                if (f.sha === "0000000000000000000000000000000000000000")
+                    continue;
                 // create the fileinfo for the commit
                 let fileInfo = {
                     filename : f.filename,
                     status : f.status
                 };
-                // add the hash of the file and schedule the snapshot if the file is not deleted
-                if (f.status !== "deleted") {
+                // if the file is renamed, keep the previous filename as well
+                if (f.status === "renamed") 
+                    fileInfo.previous_filename = f.previous_filename;
+                // add the hash of the file and schedule the snapshot if the file is not deleted or renamed
+                if (f.status !== "removed" && f.status !== "renamed") {
+                    if (f.raw_url === "https://github.com/nborracha/titanium_mobile/raw/9eeefe61b96c9dbff911fff2a0e6d88e4e9b104a/mobileweb/cli/commands/_run.js")
+                        console.log("here");
+                    // if no hash for the file snapshot, we are not interested
+                    if (!f.sha)
+                        continue;
                     fileInfo.hash = f.sha
                     Q.unshift({
                         kind : "snapshot",
@@ -211,39 +289,52 @@ function TaskCommit(task, callback) {
                 // add the fileinfo to the commit files
                 commit.files.push(fileInfo);
             }
-            // TODO store the commit information
-            callback();
-        },
-        Error(callback)
+            // store the commit information
+            SaveCommit(project, commit, callback);
+        }
     );
 }
 
-
-let snapshotIdx_ = 0;
 
 /** Obtain the snapshot of the file. */
 function TaskSnapshot(task, callback) {
-    // do every 10th file only, which should be what we endup with
-    if (snapshotIdx_++ != 10) {
-        callback();
-        return;
-    }
-    snapshotIdx_ = 0;
-    // TODO this needs to be much more clever
-    APIRequest(task.url, 
-        (response, result) => {
-            callback();
-        },
-        Error(callback),
-        false // no JSON
-    );
+    ++ stats_files;
+    // first see if we already have the snapshot
+    let subdir1 = task.hash.substr(0, 2);
+    let subdir2 = task.hash.substr(2, 2);
+    let snapshotPath = snapshotOutputDir + "/" + subdir1 + "/" + subdir2 + "/" + task.hash;
+    fs.access(snapshotPath, fs.constants.R_OK, (err) => {
+        if (err) {
+            // if the snapshot does not exist, make first sure that the path exists
+            mkdir(snapshotOutputDir, subdir1, (err) => {
+                // TODO handle error
+                mkdir(snapshotOutputDir + "/" + subdir1, subdir2, (err) => {
+                    // TODO handle error
+                    APIRequest(
+                        task.url, 
+                        (error, response, result) => {
+                            // TODO handle error
+                            fs.writeFile(snapshotPath, result, (err) => {
+                                ++stats_snapshots;
+                                callback(err);
+                            });
+                        },
+                        false // no JSON
+                    );
+                });
+            });
+        } else {
+            // there was no error, the snapshot already exists, no need to download it
+            callback(null);
+        }
+    });
 }
 
 
 
 
-var apiTokens_ = null;
-var apiTokenIndex_ = 0;
+let apiTokens_ = null;
+let apiTokenIndex_ = 0;
 
 /** Since the github  */
 function APIFullRequest(url, onDone, onError, per_page = 100) {
@@ -271,16 +362,9 @@ function APIFullRequest(url, onDone, onError, per_page = 100) {
         cont,
         onError
     );
-
-    //response.headers["link"]
-
-
-
-
-
 }
 
-function APIRequest(url, onDone, onError, json = true) {
+function APIRequest(url, onDone, json = true, retries = 10) {
     // rotate the api tokens to circumvent the 5000 requests per hour github limit
     let token = apiTokens[apiTokenIndex_++];
     if (apiTokenIndex_ == apiTokens.length)
@@ -296,12 +380,21 @@ function APIRequest(url, onDone, onError, json = true) {
     };
     // call request, async
     request(options, (error, response, body) => {
+        // first see if we should retry the request  && error.code == "ETIMEDOUT"
+        if (error) {
+            if (retries > 0) {
+                console.log(url + " -- retry " + retries);
+                APIRequest(url, onDone, json, retries - 1);
+                return;
+            } 
+        }
+        // if not proceed as normally
         if (error || response.statusCode != 200) {
             console.log(url + " -- error");
-            onError(error, response, body);
+            onDone(error, response, body);
         } else {
-            console.log(url + " -- ok");
-            onDone(response, body);
+            //console.log(url + " -- ok");
+            onDone(null, response, body);
         }
     });
 }
